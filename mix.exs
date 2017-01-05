ce6848302bff5469061c6cba11483a425e870f96
Yaroslav Zemlyanuhin <d4rk5eed@yandex.ru>
2017-01-05 23:53:52 +0300

    version update to 0.2.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule DeClone.Mixfile do
 
   def project do
     [app: :de_clone,
-     version: "0.0.1",
+     version: "0.2.0",
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
