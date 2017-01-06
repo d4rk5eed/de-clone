fda6b03e4050591f1eadaa70dd30f118ede4b924
Yaroslav Zemlyanuhin <d4rk5eed@yandex.ru>
2017-01-06 10:16:41 +0300

    - making mobile first

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule DeClone.Mixfile do
 
   def project do
     [app: :de_clone,
-     version: "0.2.0",
+     version: "0.2.1",
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
